6e4803151d603692fa050f7c91256f677b1affe6
hampter <hampter@Ubuntu.myguest.virtualbox.org>
2025-01-29 19:23:34 -0700

    fixed running make in the wrong directory

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -108,17 +108,15 @@ sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
 sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # Clean and build the writer utility
+cd $FINDERDIR
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
 echo "Validating compilation of writer utility"
-ls -l
-pwd
 find . -name "writer"
 
 # Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-cd $FINDERDIR
 mkdir -p ${OUTDIR}/rootfs/home
 cp writer ${OUTDIR}/rootfs/home/
 cp finder.sh ${OUTDIR}/rootfs/home/
