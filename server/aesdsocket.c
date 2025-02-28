d9a11806d8fef229b4030f24d20407a7596248b5
hampter <hampter@Ubuntu.myguest.virtualbox.org>
2025-02-27 18:03:39 -0700

    initial implementation of threading into socket server

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,8 @@
 // Socket program for AESD assignment 5
 // Author: Eric Percin, 2/11/2025
 // References: https://www.gta.ufrj.br/ensino/eel878/sockets
+//             https://blog.taborkelly.net/programming/c/2016/01/09/sys-queue-example.html
+//             https://man7.org/linux/man-pages/man3/list.3.html
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -13,16 +15,36 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <stdbool.h>
-
+#include <pthread.h>
+#include <sys/queue.h>
+#include <stdlib.h>
 
 
 #define DATA_FILE_PATH "/var/tmp/aesdsocketdata"
 #define INITIAL_BUFFER_SIZE 512
 
+#define DEBUG
+#ifdef DEBUG
+    #define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+    #define DEBUG_PRINT(...) 
+#endif
+
+
 // Global variables to be closed in singal handler
 int g_my_socket = -1;
 int g_my_file_write = -1;
-int g_exit_flag = 0;
+volatile int g_exit_flag = 0;
+pthread_mutex_t g_write_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Structure for an entry within the singly linked thread list
+struct thread_entry {
+    pthread_t thread_id;
+    int my_client;
+    SLIST_ENTRY(thread_entry) next_slist_entry; 
+};
+
+SLIST_HEAD(thread_head, thread_entry);
 
 
 // Helper function to close open resources before exiting
@@ -31,6 +53,7 @@ void cleanup() {
         close(g_my_file_write);
     }
     remove(DATA_FILE_PATH);
+    pthread_mutex_destroy(&g_write_mutex);
     closelog();
 }
 
@@ -39,7 +62,6 @@ void cleanup() {
 void signal_handler(int signo) {
     if (g_my_socket != -1) {
         shutdown(g_my_socket, SHUT_RDWR); 
-        close(g_my_socket);
     }
     g_exit_flag = 1;
 }
@@ -60,6 +82,8 @@ void handle_connection(int my_client, int g_my_file_write) {
             }
         }
 
+        DEBUG_PRINT("my client: %d\n",my_client);
+
         ssize_t bytes_received = recv(my_client, packet_buffer + packet_length, INITIAL_BUFFER_SIZE, 0);
         if (bytes_received <= 0) {
             perror("Call to recv() failed");
@@ -69,10 +93,16 @@ void handle_connection(int my_client, int g_my_file_write) {
 
         char *newline = memchr(packet_buffer, '\n', packet_length);
         if (newline) {
+        
+            pthread_mutex_lock(&g_write_mutex); // Lock before the write
+        
             if (write(g_my_file_write, packet_buffer, packet_length) != (ssize_t)packet_length) {
                 perror("Call to write() failed");
+                pthread_mutex_unlock(&g_write_mutex);
                 break;
             }
+            
+            pthread_mutex_unlock(&g_write_mutex); // Unlock after the write
 
             my_file_read = open(DATA_FILE_PATH, O_RDONLY);
             if (my_file_read < 0) {
@@ -89,6 +119,7 @@ void handle_connection(int my_client, int g_my_file_write) {
                     break;
                 }
             }
+            
             close(my_file_read);
             if (reader_bytes_read < 0) {
                 perror("Call to read() failed");
@@ -115,7 +146,16 @@ void handle_connection(int my_client, int g_my_file_write) {
     if (my_file_read != -1) {
         close(my_file_read);
     }
-    close(my_client);
+}
+
+
+// Wrapper function for each thread to run to handle a connection
+void *thread_connection_wrapper(void *arg) {
+    struct thread_entry *my_entry = (struct thread_entry *)arg;
+    handle_connection(my_entry->my_client, g_my_file_write);
+    close(my_entry->my_client);
+    free(my_entry);
+    pthread_exit(NULL);
 }
 
 
@@ -216,9 +256,21 @@ int main(int argc, char *argv[]) {
     printf("Listening on port 9000...\n");
     openlog("aesdsocket", LOG_PID, LOG_USER);
 
+
+    struct thread_head head;
+    SLIST_INIT(&head);
+
+
     // Infinite loop to repeatedly accept and handle clients
     while (!g_exit_flag) {
         socklen_t client_addr_len = sizeof(my_client_addr);
+        
+        struct thread_entry *current_entry = malloc(sizeof(struct thread_entry));
+        if (!current_entry) {
+            perror("Call to malloc() failed for linked list entry creation");
+            break;
+        }
+        
         my_client = accept(g_my_socket, (struct sockaddr *)&my_client_addr, &client_addr_len);
         if (my_client == -1) {
             if (g_exit_flag) {
@@ -227,18 +279,60 @@ int main(int argc, char *argv[]) {
             perror("Call to accept() failed");
             break;
         }
+        current_entry->my_client = my_client;
+        DEBUG_PRINT("Current entry client: %d\n",my_client);
+        my_client = -1;
 
         syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(my_client_addr.sin_addr));
         printf("Accepted connection from %s\n", inet_ntoa(my_client_addr.sin_addr));
 
-        handle_connection(my_client, g_my_file_write);
+        SLIST_INSERT_HEAD(&head, current_entry, next_slist_entry);
+        
+        // Create a new thread in the linked list
+        if (pthread_create(&current_entry->thread_id, NULL, thread_connection_wrapper, current_entry) != 0){
+            perror("Call to pthread_create() failed");
+            SLIST_REMOVE(&head, current_entry, thread_entry, next_slist_entry);
+            free(current_entry);
+            continue;
+        }
 
         syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(my_client_addr.sin_addr));
         printf("Closed connection from %s\n", inet_ntoa(my_client_addr.sin_addr));
+        
+        // TODO: figure out how to properly join any completed threads by traversing the list
+        // Rewatch videos?
+        
+        // Join any completed threads by traversing the list
+        /*
+        struct thread_entry *current, *next;
+        current = SLIST_FIRST(&head);
+        while (current != NULL) {
+            next = SLIST_NEXT(current, next_slist_entry);  
+            pthread_join(current->thread_id, NULL);  
+            SLIST_REMOVE(&head, current, thread_entry, next_slist_entry); 
+            free(current);  
+            current = next;  
+        }
+        */
+        
     }
 
     syslog(LOG_INFO, "Caught signal, exiting");
     printf("Caught signal, exiting\n");
+    
+    // TODO: figure out how to properly join and free remaining threads
+    /*
+    struct thread_entry *current, *next;
+    current = SLIST_FIRST(&head);
+    while (current != NULL) {
+        next = SLIST_NEXT(current, next_slist_entry);  
+        pthread_join(current->thread_id, NULL);  
+        SLIST_REMOVE(&head, current, thread_entry, next_slist_entry); 
+        free(current);  
+        current = next;  
+    }
+    */
+    
     cleanup();
     
     return 0;
